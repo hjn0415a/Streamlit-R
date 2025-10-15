8ffc8f7743b55d2611fe2e00709c23952178e875
Yoo HoJun <hjn0415a@gmail.com>
2025-10-15 15:02:58 +0900

    chore: disable Enrichkegg section in Streamlit navigation

--- app.py
+++ app.py
@@ -33,11 +33,11 @@ if __name__ == '__main__':
             st.Page(Path("content", "rcode", "ridgeplot.py"), title="Ridgeplot", icon="🕸️"),
             st.Page(Path("content", "rcode", "pathway_gene.py"), title="Heatmaplike functional classification", icon="📋"),
         ],
-        "Enrichkegg": [
-            st.Page(Path("content", "rcode", "enrichkegg.py"), title="Enrichkegg", icon="📋"),
-            st.Page(Path("content", "rcode", "enrichkegg_dotplot.py"), title="Enrichkegg dotplot", icon="📉"),
-            st.Page(Path("content", "rcode", "pathview.py"), title="Pathview", icon="🔎"),
-        ],
+        # "Enrichkegg": [
+        #     st.Page(Path("content", "rcode", "enrichkegg.py"), title="Enrichkegg", icon="📋"),
+        #     st.Page(Path("content", "rcode", "enrichkegg_dotplot.py"), title="Enrichkegg dotplot", icon="📉"),
+        #     st.Page(Path("content", "rcode", "pathview.py"), title="Pathview", icon="🔎"),
+        # ],
     }
 
     pg = st.navigation(pages)
